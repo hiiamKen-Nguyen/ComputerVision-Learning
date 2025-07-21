d6b642a45b5675382ef0645716725f0c43bac4de
hiiamKen-Nguyen <nguyenlong16203@gmail.com>
2025-07-21 21:40:50 +0700

    done day 2 with basic function, resize img, wrap prepective, joining img

---
+++ day2/5.wrap_prespective.py
@@ -0,0 +1,20 @@
+import cv2 
+import numpy as np
+
+width, height = 250, 350
+
+img = cv2.imread("resources/cards.jpg")
+
+pts1 = np.float32([[752,118], [1120,265], [540,668], [871,830] ])
+pts2 = np.float32([[0,0], [width, 0], [0, height], [width, height]])
+
+metrix = cv2.getPerspectiveTransform(pts1, pts2)
+img_out = cv2.warpPerspective(img, metrix, (width, height))
+
+
+cv2.imshow('cards', img)
+cv2.imshow("card wrap", img_out)
+
+
+
+cv2.waitKey(0)
---
+++ day2/README.md
@@ -0,0 +1,22 @@
+Create anaconda environment: 
+B1: open Anacoda promt
+
+B2: figure out exact file locaion that you want initial anacoda envir: cd /d D:\ComputerVisionLS\day3
+
+B3: code .
+
+B4: conda create cvdev -n python=3.11 -y
+
+
+
+
+pip install -r requirement.txt
+
+
+
+
+
+
+
+
+
